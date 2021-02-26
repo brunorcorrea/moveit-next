a3bc24c45386c3912785952965d37522869f9dc4
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-02-26 15:27:07 -0300

    Finalização da Trilha React

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,16 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile(){
-    return(
-        <div className={styles.profileContainer}>
-          <img src="http://github.com/brunorcorrea.png" alt="Bruno Ricardo Corrêa" />
-          <div>
-            <strong>Bruno Ricardo Corrêa</strong>
-            <p>
-              <img src="icons/level.svg" alt="Level" />
-              Level 1
-            </p>
-          </div>
+  const { level } = useContext(ChallengesContext);
+  
+  return(
+      <div className={styles.profileContainer}>
+        <img src="http://github.com/brunorcorrea.png" alt="Bruno Ricardo Corrêa" />
+        <div>
+          <strong>Bruno Ricardo Corrêa</strong>
+          <p>
+            <img src="icons/level.svg" alt="Level" />
+            Level {level}
+          </p>
         </div>
-    );
+      </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,61 @@
+import Head from 'next/head';
+import React from 'react';
+import { GetServerSideProps } from 'next';
+
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
+import { CountdownProvider } from '../contexts/CountdownContext';
 import { Profile } from '../components/Profile';
 
-import Head from 'next/head';
-
 import styles from '../styles/pages/Home.module.css';
+import { ChallengeBox } from '../components/ChallengeBox';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
-export default function Home() {
+interface HomeProps {
+  level: number; 
+  currentExperience: number;
+  challengesCompleted:number;
+}
+
+export default function Home(props) {
   return (
-    <div className={styles.container}>
-    <Head>
-      <title>Início | move.it</title>
-    </Head>
-    <ExperienceBar/>
+    <ChallengesProvider 
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted = {props.challengesCompleted}
+    >
+      <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+      <ExperienceBar/>
 
-    <section>
-      <div>
-        <Profile />
-        <CompletedChallenges />
-        <Countdown />
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
       </div>
-      <div> </div>
-    </section>
-    </div>
+    </ChallengesProvider>
   )
+}
+
+export const getServerSideProps:GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level), 
+      currentExperience: Number(currentExperience), 
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
