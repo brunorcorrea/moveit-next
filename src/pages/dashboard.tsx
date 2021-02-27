019eeae83071fe151996a701b02d6058a70e4c05
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2021-02-27 19:53:14 -0300

    Criação da Landing Page no Index

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,66 @@
+import Head from 'next/head';
+import React, { useContext, useState } from 'react';
+import Link,{ GetServerSideProps } from 'next';
+import { Redirect } from 'react-router';
+
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
+import { ExperienceBar } from '../components/ExperienceBar';
+import { CountdownProvider } from '../contexts/CountdownContext';
+import { Profile } from '../components/Profile';
+
+import styles from '../styles/pages/Home.module.css';
+import { ChallengeBox } from '../components/ChallengeBox';
+import { ChallengesProvider, ChallengesContext } from '../contexts/ChallengesContext';
+import { ProfileProvider } from '../contexts/ProfileContext';
+
+interface HomeProps {
+  level: number; 
+  currentExperience: number;
+  challengesCompleted:number;
+}
+
+export default function Home(props: HomeProps) {
+  return (
+    <ChallengesProvider 
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted = {props.challengesCompleted}
+    >
+      <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+      <ExperienceBar/>
+
+      <CountdownProvider>
+        <section>
+          <div>
+          <ProfileProvider>
+              <Profile />
+          </ProfileProvider>
+            
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
+      </div>
+    </ChallengesProvider>
+  );
+}
+
+export const getServerSideProps:GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level), 
+      currentExperience: Number(currentExperience), 
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,75 +1,59 @@
-import Head from 'next/head';
-import React, { useContext, useState } from 'react';
-import Link,{ GetServerSideProps } from 'next';
-import { Redirect } from 'react-router';
+import { useContext, useState } from 'react';
+import Link from 'next/link';
+import styles from '../styles/pages/LandingPage.module.css';
 
-import { CompletedChallenges } from '../components/CompletedChallenges';
-import { Countdown } from '../components/Countdown';
-import { ExperienceBar } from '../components/ExperienceBar';
-import { CountdownProvider } from '../contexts/CountdownContext';
-import { Profile } from '../components/Profile';
+export default function LandingPage(){
+  const [inputValue,setInputValue] = useState("");
 
-import styles from '../styles/pages/Home.module.css';
-import { ChallengeBox } from '../components/ChallengeBox';
-import { ChallengesProvider, ChallengesContext } from '../contexts/ChallengesContext';
-import  LandingPage  from './LandingPage';
-import { ProfileProvider } from '../contexts/ProfileContext';
+  function handleSubmit(event){
+    event.preventDefault();
+    localStorage.setItem('name', inputValue);
+    localStorage.setItem('picture', `https://github.com/${inputValue}.png`);
+    if(inputValue !== "") redirectToDashboard();
+  }
 
-interface HomeProps {
-  level: number; 
-  currentExperience: number;
-  challengesCompleted:number;
-}
+  function redirectToDashboard(){
+    window.location.href = "/dashboard";
+  }
 
-export default function Home(props: HomeProps) {
   const user = typeof window !== 'undefined' ? localStorage.getItem('name') : null;
-
-  if(!user){
-    if(typeof window !== 'undefined'){
-      window.location.href = "/LandingPage"
-    }
-  }
-
   return (
-    <ChallengesProvider 
-      level={props.level}
-      currentExperience={props.currentExperience}
-      challengesCompleted = {props.challengesCompleted}
-    >
-      <div className={styles.container}>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
-      <ExperienceBar/>
+    <div className={styles.landingPageContainer}>
+        <img src="./background-image.svg" alt="Logo" className={ styles.backgroundImage }/>
+      <div className={ styles.loginArea }>
+        <img src="./logo-full.svg" alt="Logo" className={ styles.logoFull }/>
 
-      <CountdownProvider>
-        <section>
-          <div>
-          <ProfileProvider>
-              <Profile />
-          </ProfileProvider>
-            
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
+        <strong>Bem-vindo</strong>
+
+        <img src="./git-message.svg" alt="Mensagem do git" className={ styles.gitMessage }/>
+
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input 
+              type="text" 
+              placeholder="Digite seu username"
+              onChange={e => setInputValue(e.target.value)}
+            />
+            <button 
+              type="submit"
+              className={styles.logIn}
+            >
+              <img src="./icons/arrow.svg" alt="Entrar" />
+            </button>
+          </form>
+        </div>
+        <div>
+          { user && (
+            <button 
+              type="submit"
+              className={styles.alreadyLoggedIn}
+              onClick={redirectToDashboard}
+            >
+              logar como {user}
+            </button>
+          )}
+        </div>
       </div>
-    </ChallengesProvider>
+    </div>
   );
-}
-
-export const getServerSideProps:GetServerSideProps = async (ctx) => {
-  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
-
-  return {
-    props: {
-      level: Number(level), 
-      currentExperience: Number(currentExperience), 
-      challengesCompleted: Number(challengesCompleted)
-    }
-  }
 }
